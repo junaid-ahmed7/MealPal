c8a90930f811951f11719a075bcad4eb856df287
Junaid Ahmed <junaid7ahmed96@gmail.com>
2023-05-17 15:53:02 -0700

    Initial Commit

---
+++ MealPal/MealPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPalApp.swift
+//  MealPal
+//
+//  Created by Junaid Ahmed on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MealPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
